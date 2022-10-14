66abd0e6b57d5d5148ead7dae5850a4f3bf53df8
jennhuck <jah2ax@virginia.edu>
2022-10-14 16:24:03 -0400

    Minor tweaks for fall 2022 workshop

--- scripts/01-penguins.R
+++ scripts/01-penguins.R
@@ -1,8 +1,13 @@
 # Reproducible Analysis and Documentation with R and RStudio
 # Penguins project
 
+# Install and load palmerpenguins
+# Note: uncomment the next line if you do not have palmerpenguins installed
+# install.packages("palmerpenguins")
+library(palmerpenguins)
+
 # Prepare workshop data----
-readr::write_csv(palmerpenguins::penguins, ("data/penguins.csv"))
+readr::write_csv(penguins, ("data/penguins.csv"))
 
 # Note that this is a basic .R script.  We will look at R Markdown (.Rmd) files
 # next.
